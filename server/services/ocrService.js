617aa91dea566fa6a57731e70b317975b9c92401
Durgaprasd25d <durgaprasaddalai10@gmail.com>
2026-07-29 00:25:09 +0530

    Fix Vercel serverless Tesseract execution by using /tmp cachePath without child workers

--- server/services/ocrService.js
+++ server/services/ocrService.js
@@ -1,25 +1,8 @@
 const Tesseract = require("tesseract.js");
 
-let workerInstance = null;
-
-async function getWorker() {
-    if (!workerInstance) {
-        workerInstance = await Tesseract.createWorker("eng", 1, {
-            logger: () => {},
-            errorHandler: (err) => console.error("Tesseract worker error:", err)
-        });
-    }
-    return workerInstance;
-}
-
 exports.readText = async (imageSource) => {
-    try {
-        const worker = await getWorker();
-        const { data } = await worker.recognize(imageSource);
-        return data.text;
-    } catch (err) {
-        console.warn("Worker recognize failed, falling back to direct recognize:", err.message);
-        const { data } = await Tesseract.recognize(imageSource, "eng");
-        return data.text;
-    }
+    const { data } = await Tesseract.recognize(imageSource, "eng", {
+        cachePath: "/tmp"
+    });
+    return data.text;
 };
